d82afdb67fa2ad68771f834957ee6c5127dce4d6
sathish <rajsathisg775@gmail.com>
2025-08-10 22:04:32 +0530

    Add Contact

--- src/Sections/Intro/Intro.jsx
+++ src/Sections/Intro/Intro.jsx
@@ -1,6 +1,9 @@
 import "./Intro.scss";
 import heroImg from "../../assets/Profile_img.png";
 import Button from "../../Component/Button/Button";
+import { MdOutlineMail } from "react-icons/md";
+import { FiPhoneCall } from "react-icons/fi";
+
 
  function Intro() {
 
@@ -17,6 +20,16 @@ import Button from "../../Component/Button/Button";
         <div className="hero__text">
           <h1>Hi, I'm <span>Sathish R</span></h1>
           <p>React JS Developer | 2 + Years Experience</p>
+          <div className="hero__contact">
+            <label>
+              <MdOutlineMail className="icon" />
+              rajsathish775@gmail.com
+            </label>
+             <label>
+              <FiPhoneCall className="icon"  />
+              9894685596
+            </label>
+          </div>
           <a href="#projects  ">
              <Button onClick={HandleView} buttonName = "View My Work" />
              <a href="#projects"  id="Hiddentag" style={{visibility:"hidden"}}></a>
